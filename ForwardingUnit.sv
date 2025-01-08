2a4f5ea9094da54e4538fc00bc35533d448a7eda
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2025-01-08 16:22:12 +0800

    feat: forward from alu

--- ForwardingUnit.sv
+++ ForwardingUnit.sv
@@ -13,6 +13,9 @@ module ForwardingUnit #(
     input ID_EX_Register id_ex_reg,
     input EX_MEM_Register ex_mem_reg,
     input MEM_WB_Register mem_wb_reg,
+    input logic use_alu_result,
+    input logic [31:0] alu_result0,
+    input logic [31:0] alu_result1,
     input logic mem_read_in_mem,
     input logic [31:0] mem_read_result,
     output logic [31:0] forwarded_data,
@@ -33,10 +36,16 @@ module ForwardingUnit #(
                 id_ex_reg.ctrl0.reg_write && 
                 id_ex_reg.valid0 &&
                 id_ex_reg.reg_write_addr0 == required_reg) begin
-                    forwarded_data = id_ex_reg.reg_write_data0;
                     data_ready_after_stage = get_data_ready_stage(id_ex_reg.ctrl0.reg_write_data_src);
-                    stall = (data_ready_after_stage > PipelineStage_ID) && (PIPELINE_STAGE == required_stage);
-                    forwarding_type = FORWARDING_TYPE_ID_EX;
+                    if (use_alu_result && id_ex_reg.ctrl0.reg_write_data_src == REG_WRITE_DATA_SOURCE_ALU) begin
+                        forwarded_data = alu_result0;
+                        stall = 0;
+                        forwarding_type = FORWARDING_TYPE_ID_EX;
+                    end else begin
+                        forwarded_data = id_ex_reg.reg_write_data0;
+                        stall = (data_ready_after_stage > PipelineStage_ID) && (PIPELINE_STAGE == required_stage);
+                        forwarding_type = FORWARDING_TYPE_ID_EX;
+                    end
                     forwarded_from = 32'd0;
             end 
             // 检查 ID/EX 阶段的第二条指令
@@ -44,10 +53,16 @@ module ForwardingUnit #(
                 id_ex_reg.ctrl1.reg_write && 
                 id_ex_reg.valid1 &&  // 第二条指令必须有效
                 id_ex_reg.reg_write_addr1 == required_reg) begin
-                    forwarded_data = id_ex_reg.reg_write_data1;
                     data_ready_after_stage = get_data_ready_stage(id_ex_reg.ctrl1.reg_write_data_src);
-                    stall = (data_ready_after_stage > PipelineStage_ID) && (PIPELINE_STAGE == required_stage);
-                    forwarding_type = FORWARDING_TYPE_ID_EX;
+                    if (use_alu_result && id_ex_reg.ctrl1.reg_write_data_src == REG_WRITE_DATA_SOURCE_ALU) begin
+                        forwarded_data = alu_result1;
+                        stall = 0;
+                        forwarding_type = FORWARDING_TYPE_ID_EX;
+                    end else begin
+                        forwarded_data = id_ex_reg.reg_write_data1;
+                        stall = (data_ready_after_stage > PipelineStage_ID) && (PIPELINE_STAGE == required_stage);
+                        forwarding_type = FORWARDING_TYPE_ID_EX;
+                    end
                     forwarded_from = 32'd1;
             end 
             // 检查 EX/MEM 阶段的第一条指令
